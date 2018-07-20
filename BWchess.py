87397515d2ea89c5dda90cbd288c436eea2de242
tzz <zzt1201@gmail.com>
2018-07-20 18:44:00 +0800

    without check

--- BWchess.py
+++ BWchess.py
@@ -8,26 +8,28 @@ class BWchess():
         self.chess_board = np.zeros((8, 8), dtype=np.int8)
         self.chess_board[3, 3], self.chess_board[4, 4] = 1, 1
         self.chess_board[3, 4], self.chess_board[4, 3] = -1, -1
+        self.round_counter = 0
 
-        round_counter = 0
-
-        while True:
-            coord = [int(i) for i in input().replace(' ', '')]
-            # counter remain if coord is not available
-            if self.put_chess(*coord, 1 if round_counter % 2 == 0 else -1):
-                round_counter += 1
-            print(self.chess_board)
+        # while True:
+        #     coord = [int(i) for i in input().replace(' ', '')]
+        #     # counter remain if coord is not available
+        #     if self.put_chess(*coord, 1 if self.round_counter % 2 == 0 else -1):
+        #         self.round_counter += 1
+        #     print(self.chess_board)
 
     def put_chess(self, x, y, identity):
         if self.chess_board[x, y] == 0:
             self.chess_board[x, y] = identity
 
-        # put check
-        condition = self.reverse_chess(x, y, identity)
-        if condition:
-            return True
+            # put check
+            condition = self.reverse_chess(x, y, identity)
+            if condition:
+                return True
+            else:
+                self.chess_board[x, y] = 0
+                return False
+                
         else:
-            self.chess_board[x, y] = 0
             return False
 
     def reverse_chess(self, x, y, identity):
@@ -35,29 +37,32 @@ class BWchess():
         S = self.chess_board[:, y]
         H = self.chess_board[x, :]
         X = np.diag(self.chess_board, y - x)
-        F = np.diag(self.chess_board[:, ::-1], x - (y + 1))
+        F = np.diag(self.chess_board[:, ::-1],(7 - y) - x)
 
         # feedback info for put check
         condition = False
 
         # reverse the chess
         for line in [S[:x][::-1], S[x+1:], H[:y][::-1], H[y+1:], \
-                     X[:x][::-1], X[x+1:], F[:y+1][::-1], F[(y+1)+1:]]:
+                     X[:min(x, y)][::-1], X[min(x, y)+1:], \
+                     F[:min(x, 7-y)][::-1], F[min(x, y-1)+1:]]:
+
             # make np.array writeable when represented as line
             line.flags.writeable = True
 
             for i, chess in enumerate(line):
                 if chess == 0 and i == 0:
                     break
-                elif chess == 0 and i != 0:
-                    if line[i-1] == identity:
-                        line[:i] = identity
-                        condition = True
+                elif chess == 0 and i > 1:
+                    for j in range(1, i):
+                        if line[i-j] == identity:
+                            line[:i-j] = identity
+                            condition = True
                     break
 
         return condition
 
-    def check(self):
+    def check_winner(self):
         pass
 
 if __name__ == "__main__":
---
+++ BWchessGui.py
@@ -0,0 +1,55 @@
+import math
+import tkinter as tk
+from BWchess import BWchess
+
+class BWchessEnv(tk.Tk):
+    def __init__(self):
+        # initialize BWchess
+        self.chess = BWchess()
+
+        # initialize tk
+        super().__init__()
+        self.title('Black & White Chess')
+        self.geometry('400x400')
+        self.__setup_canvas()
+
+        # start window
+        self.mainloop()
+
+    def __setup_canvas(self):
+        self.canvas = tk.Canvas(self, bg='Tan', width=400, height=400)
+        self.canvas.pack()
+        self.canvas.bind('<Button-1>', self.__scheduler)
+        self.__update_board()
+
+    def __update_board(self):
+        self.canvas.delete('all')
+
+        for i in range(50, 400, 50):
+            # vertical line
+            self.canvas.create_line(i, 0, i, 400)
+            # horizontal line
+            self.canvas.create_line(0, i, 400, i)
+
+        for i in range(8):
+            for j in range(8):
+                if self.chess.chess_board[i, j] != 0:
+                    self.canvas.create_oval(j * 50 + 5, i * 50 + 5, \
+                                            j * 50 + 45, i * 50 + 45, \
+                                            fill='black' if self.chess.chess_board[i, j] == 1 else 'white')
+        self.update()
+
+    def __scheduler(self, event):
+        # calculate coordinates and id
+        x = math.floor(event.y / 50)
+        y = math.floor(event.x / 50)
+        identity = 1 if self.chess.round_counter % 2 == 0 else -1
+
+        # put chess
+        if self.chess.put_chess(x, y, identity):
+            self.chess.round_counter += 1
+            self.__update_board()
+        
+
+if __name__ == '__main__':
+    BWchessEnv()
