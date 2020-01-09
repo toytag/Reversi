1ee3efe208faf1c4b8dab8ec30c6eb2333beb796
Zhenzhong Tang <tangzhzh@shanghaitech.edu.cn>
2020-01-08 18:23:57 -0800

    Update reversiGUI.py rename file get some function from reversi.py

--- reversiGui.py
+++ reversiGui.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 from reversi import reversi
 
 
-class reversiEnv(tk.Tk):
+class reversiGUI(tk.Tk):
     def __init__(self):
         # initialize reversi
         self.chess = reversi()
@@ -19,72 +19,72 @@ class reversiEnv(tk.Tk):
         self.mainloop()
 
     def __setup_canvas(self):
-        self.canvas = tk.Canvas(self, bg='Tan', width=400, height=400)
+        self.canvas = tk.Canvas(self, bg='BurlyWood', width=400, height=400)
         self.canvas.pack()
         self.canvas.bind('<Button-1>', self.__scheduler)
         for i in range(50, 400, 50):
-            # vertical line
+            # the vertical line
             self.canvas.create_line(i, 0, i, 400)
-            # horizontal line
+            # the horizontal line
             self.canvas.create_line(0, i, 400, i)
         self.__update_board()
 
     def __update_board(self):
         self.canvas.delete('chess', 'avai')
 
-        identity = 1 if self.chess.round_counter % 2 == 0 else -1
+        player = reversi.BLACK \
+            if self.chess.round_count % 2 == 0 else reversi.WHITE
 
         for i in range(8):
             for j in range(8):
-                if self.chess.available[i, j] in [identity, 2]:
+                if self.chess.available[i, j] in [player, reversi.BOTH]:
                     self.canvas.create_oval(
                         j * 50 + 20, i * 50 + 20,
                         j * 50 + 30, i * 50 + 30,
-                        outline='lightseagreen',
-                        fill='lightseagreen',
+                        outline='cyan',
+                        fill='cyan',
                         tags='avai',
                     )
                 if self.chess.chess_board[i, j] != 0:
                     self.canvas.create_oval(
                         j * 50 + 5, i * 50 + 5,
                         j * 50 + 45, i * 50 + 45,
-                        fill='black' if self.chess.chess_board[i, j] == 1 else 'white',
+                        fill={
+                            reversi.BLACK: 'black',
+                            reversi.WHITE: 'white',
+                        }.get(self.chess.chess_board[i, j]),
                         tags='chess',
                     )
                     
         self.update()
 
     def __scheduler(self, event):
-        # calculate coordinates and id
+        # calculate coordinates and player
         x = math.floor(event.y / 50)
         y = math.floor(event.x / 50)
-        identity = self.chess.player_black \
-                   if self.chess.round_counter % 2 == 0 \
-                   else self.chess.player_white
+        player = reversi.BLACK \
+            if self.chess.round_count % 2 == 0 else reversi.WHITE
 
         # put chess
-        if self.chess.put_chess(x, y, identity):
-            self.chess.round_counter += 1
-            status = self.chess.check_status()
+        if self.chess.put_chess(x, y, player):
+            self.chess.check_status()
             self.__update_board()
-            next_identity = self.chess.player_black \
-                            if self.chess.round_counter % 2 == 0 \
-                            else self.chess.player_white
-            if status == 'black skip' and next_identity == self.chess.player_black:
-                self.chess.round_counter += 1
-            elif status == 'white skip' and next_identity == self.chess.player_white:
-                self.chess.round_counter += 1
-            elif 'Black Win' in status:
-                messagebox.showinfo('Black & White Chess', status)
+
+            # for GUI, now moved to reversiGui.py
+            if self.chess.black_available_count == 0 and self.chess.white_available_count == 0:
+                if self.chess.black_count > self.chess.white_count:
+                    messagebox.showinfo('Reversi',
+                        f'Black Win\n\n{self.chess.black_count} : {self.chess.white_count}')
+                elif self.chess.black_count < self.chess.white_count:
+                    messagebox.showinfo('Reversi',
+                        f'White Win\n\n{self.chess.black_count} : {self.chess.white_count}')
+                else:
+                    messagebox.showinfo('Reversi', 'Tie')
                 self.destroy()
-            elif 'White Win' in status:
-                messagebox.showinfo('Black & White Chess', status)
-                self.destroy()
-            elif status == 'Tie':
-                messagebox.showinfo('Black & White Chess', status)
-                self.destroy()
+                exit()
+                
         self.__update_board()
         
 
 if __name__ == '__main__':
-    reversiEnv()
+    reversiGUI()
