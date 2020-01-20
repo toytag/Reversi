d2ecb65f35b688336cd169d5b983d869af281542
Zhenzhong Tang <tangzhzh@shanghaitech.edu.cn>
2020-01-20 14:12:33 -0800

    Fix boolean type error on linux, add test

--- reversi.py
+++ reversi.py
@@ -23,16 +23,52 @@ class reversi(REVERSI):
         _libreversi.init(byref(self))
 
     def put_chess(self, x, y, player):
-        res = _libreversi.put_chess(byref(self), x, y, player)
-        return True if res == 1 else False
+        return _libreversi.put_chess(byref(self), x, y, player)
 
     def check_status(self):
         _libreversi.check_status(byref(self))
 
     def is_end(self):
-        res = _libreversi.is_end(self)
-        return True if res == 1 else False
+        return _libreversi.is_end(self)
 
     def minimax(self, depth, player):
         best_move = _libreversi.minimax_parallel(self, depth, player)
         return divmod(best_move, 8)
+
+def print_board(board):
+    for i in range(8):
+        for j in range(8):
+            print(board[i][j], end=' ')
+        print()
+
+def inspect(env):
+    print()
+    print("chess_board:")
+    print_board(env.chess_board)
+    print("avl_board:")
+    print_board(env.avl_board)
+    print(f"black_count:\t {env.black_count}")
+    print(f"black_avl_count: {env.black_avl_count}")
+    print(f"white_count:\t {env.white_count}")
+    print(f"white_avl_count: {env.white_avl_count}")
+    print(f"round_count:\t {env.round_count}")
+    print()
+
+if __name__ == "__main__":
+    # run test
+    env = reversi()
+
+    skip_count = 0
+
+    while not env.is_end():
+        player = reversi.BLACK if env.round_count % 2 == 0 else reversi.WHITE
+        best_move = env.minimax(4, player)
+        if best_move == (3, 3):
+            skip_count += 1
+        env.put_chess(*best_move, player)
+        env.check_status()
+
+    inspect(env)
+
+    assert env.black_count + env.white_count - 4 + skip_count == env.round_count
+    print("python test success!")
